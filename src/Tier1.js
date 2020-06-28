0c8ed98be8afa03c132e3322c173288a74017586
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-06-28 03:09:02 +0000

    Done.

--- src/Tier1.js
+++ src/Tier1.js
@@ -13,14 +13,26 @@ export default class Tier1 extends Component {
       childColor: getReducedColor(initialColor)
     }
   }
+  
+  
+  handleClick = () => {
+    const color = getRandomColor()
+    this.setState({color: color, childColor: getReducedColor(color)})
+  }
+  
+  handleChildClick = (event) => {
+    event.stopPropagation()
+    this.setState({childColor: getRandomColor()})
+  }
+  
 
   render() {
     // hard coded color values have been added below, though they won't be
     // present in our solution. What should they be replaced with?
     return (
-      <div onClick={() => {this.setState({color: "#000"})}} className="tier1" style={{backgroundColor: this.state.color, color: this.state.color}}>
-        <Tier2 color={"#0F0"} />
-        <Tier2 color={"#0FF"} />
+      <div onClick={this.handleClick} className="tier1" style={{backgroundColor: this.state.color, color: this.state.color}}>
+        <Tier2 color={this.state.childColor} handleChildClick={this.handleChildClick}} />
+        <Tier2 color={this.state.childColor} handleChildClick={this.handleChildClick}} />
       </div>
     )
   }
--- src/Tier2.js
+++ src/Tier2.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { getReducedColor } from './randomColorGenerator.js'
+import { getRandomColor, getReducedColor } from './randomColorGenerator.js'
 import Tier3 from './Tier3'
 
 
@@ -11,14 +11,21 @@ export default class Tier2 extends Component {
       childColor: getReducedColor(this.props.color),
     }
   }
+  
+  
+  handleChildClick = (event) => {
+    event.stopPropagation()
+    this.setState({childColor: getRandomColor()})
+  }
+  
 
   render() {
     // hard coded color values have been added below, though they won't be
     // present in our solution. What should they be replaced with?
     return (
       <div className="tier2" style={{backgroundColor: this.props.color, color: this.props.color}}>
-        <Tier3 color={"#0F0"} />
-        <Tier3 color={"#F00"} />
+        <Tier3 color={this.state.childColor} handleChildClick={this.handleChildClick} />
+        <Tier3 color={this.state.childColor} handleChildClick={this.handleChildClick} />
       </div>
     )
   }
